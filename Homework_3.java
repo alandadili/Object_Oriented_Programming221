260bfc11f6cf919f25b6686938942b8ec9a3cefd
alandadili <alandadili123@gmail.com>
2024-11-04 20:12:13 -0500

    made file

---
+++ Homework_3.java
@@ -0,0 +1,11 @@
+public class Homework_3 
+{
+    //employee contains  2
+    //salary has 4  extend to Employee use super
+    //hourly has 4 extend to Employee use super
+    //static method to keep track of the employees
+    //counter for each salaried and hourly to keep track of how many each
+    //main method is the client class
+    //input methods to ask hourly or salary 
+    //different question for salary and hourly
+}
