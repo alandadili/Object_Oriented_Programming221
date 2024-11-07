d7ec9fa04e00bc7f3f1e581f0cfdcad1595258a8
alandadili <alandadili123@gmail.com>
2024-11-06 19:14:26 -0500

    final

--- quiz4.java
+++ quiz4.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.Stack;
+import java.util.Stack; //added stack import after finding it
 class quiz4 
 {
     public static void main(String[] args) 
@@ -7,7 +7,7 @@ class quiz4
         System.out.println("Enter string");
         Scanner sc=new Scanner(System.in);
 
-        if(sc.hasNext())
+        if(sc.hasNext())    //changed the while loop to if loop because of an infinite loop
         {
             String input=sc.next();
             System.out.println(isBalanced(input));
@@ -17,26 +17,30 @@ class quiz4
     public static boolean isBalanced(String input) 
     {
         Stack<Character> stack=new Stack<>();
-        for (int i=0;i<input.length();i++) 
+        for (int i=0;i<input.length();i++) //checks for each character in the string
         {
             char current=input.charAt(i);
-            if (current== '(' || current== '{' || current== '[') 
+            if (current== '(' || current== '{' || current== '[') //checks if it is an opening charcter
             {
-                stack.push(current);            //checks if it is an opening charcter
+                stack.push(current);    //pushes the character to the top of the stack
             } 
-            else if (current== ')' || current== '}' || current== ']') 
+            else if (current== ')' || current== '}' || current== ']') //checks if it is a closing character
             {
                 if (stack.isEmpty()) 
                 {
-                    return false;
+                    return false;   //is empty is immediately true so make it false
                 }
                 char top=stack.pop();
-                if (current == ')' && top != '('||current == '}' && top != '{' ||current == ']' && top != '[') 
+                if (current== ')' && top!= '('||current== '}' && top != '{' ||current== ']' && top!= '[') //checks previous to see if it closes
                 {
                     return false;
                 }
             }
+                else
+                {
+                    return false;   //if it is not an opening or closing character then it is false
+                }
         }
-        return stack.isEmpty();
+        return stack.isEmpty(); 
     }
 }
