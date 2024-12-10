62de58c22ea03d88302cc8cbac8f533aee07da3d
alandadili <alandadili123@gmail.com>
2024-12-09 19:09:24 -0500

    made to test code on test

---
+++ quiz5.java
@@ -0,0 +1,16 @@
+public class quiz5
+{
+    int x;
+
+    public quiz5(String t)
+    {
+        System.out.print("Test-");
+    }
+
+    public static void main(String[] args)
+    {
+        quiz5 test = new quiz5("CMPSC 221-");
+        System.out.println(test.x);
+    }
+
+}
